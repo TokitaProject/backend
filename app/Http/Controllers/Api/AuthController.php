9553ded34e0cfa36837cfb0e76807bb873437188
Wizeguh <younghans532@gmail.com>
2022-09-30 04:17:00 +0700

    update api user

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,73 +3,96 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Helper;
+use App\Models\PersonalToken;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
 
-
 class AuthController extends Controller {
 
-    public function login(Request $request) {   
-        $validasi = Validator::make($request->all(),[
-        'email' =>'required',
-        'password' =>'required|min:6',
-    ]);
+    use Helper;
 
-    if ($validasi->fails()){
-        return $this->errors($validasi->errors()->first());
-    }
+    public function login(Request $request) {
+        $validasi = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required|min:6',
+        ]);
 
-        $user = User::where('email', $request->email)->first();
-        if($user){
+        if ($validasi->fails()) {
+            return $this->error($validasi->errors()->first());
+        }
 
-            if (password_verify($request->password, $user->password)){
-                return $this->success($user, "welcome " .$user->name);
+        $user = User::where('email', $request->email)->with('userRole')->first();
+        if ($user) {
+            if (password_verify($request->password, $user->password)) {
+                $token = PersonalToken::create([
+                    'token' => $this->generateToken(),
+                    'userId' => $user->id
+                ]);
+
+                $user->token = $token->token;
+                return $this->success($user);
             } else {
-                return $this->errors("wrong password!");
+                return $this->error("Wrong password");
             }
         }
-        return $this->errors("user tidak ditemukan");
+        return $this->error("User tidak di temukan");
     }
 
-    public function register(Request $request)
-    {
-        $validasi = Validator::make($request->all(),[
-        'name' =>'required',
-        'email' =>'required|unique:users',
-        'phone' =>'required|unique:users',
-        'password' =>'required|min:6',
-    ]);
+    public function register(Request $request) {
+        $validasi = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|unique:users',
+            'phone' => 'required|unique:users',
+            'password' => 'required|min:6',
+        ]);
 
-    
-    if ($validasi->fails()){
-        return $this->errors($validasi->errors()->first());
-    }
+        if ($validasi->fails()) {
+            return $this->error($validasi->errors()->first());
+        }
 
-    $user = User::create(array_merge($request->all(), [
-        'password' => bcrypt($request->password)
-    ]));
-    if ($user) {
-        return $this->success($user, 'welcome ' . $user->name);
-    } else {
-        return $this->errors("Something wrong!");
-    }
-    }
-    
-    public function success($data, $message = "success")
-    {
-        return response()->json([
-            'code' => 200,
-            'message' => $message,
-            'data'  => $data
-        ], 200);
+        $user = User::create(array_merge($request->all(), [
+            'password' => bcrypt($request->password)
+        ]));
+
+        if ($user) {
+            return $this->success($user, 'selamat datang ' . $user->name);
+        } else {
+            return $this->error("Terjadi kesalahan");
+        }
     }
 
-    public function errors($message)
-    {
-        return response()->json([
-            'code' => 400,
-            'message' => $message
-        ], 400);
+    public function update(Request $request, $id) {
+
+        $user = User::where('id', $id)->first();
+        if ($user) {
+            $user->update($request->all());
+            return $this->success($user);
+        }
+
+        return $this->error("tidak ada user");
+    }
+
+    public function upload(Request $request, $id) {
+        $user = User::where('id', $id)->first();
+        if ($user) {
+            $fileName = "";
+            if ($request->image) {
+                $image = $request->image->getClientOriginalName();
+                $image = str_replace(' ', '', $image);
+                $image = date('Hs') . rand(1, 999) . "_" . $image;
+                $fileName = $image;
+                $request->image->storeAs('public/user', $image);
+            } else {
+                return $this->error("Image wajib di kirim");
+            }
+
+            $user->update([
+                'image' => $fileName
+            ]);
+            return $this->success($user);
+        }
+        return $this->error("User tidak ditemukan");
     }
 }
